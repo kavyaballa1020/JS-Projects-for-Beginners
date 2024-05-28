9b0af28b840eaaf7c92635d76afa5a7667f71b70
Balla Kavya <ballakavya594@gmail.com>
2024-05-28 08:16:24 +0530

    Update | App.js

--- PreLoader/app.js
+++ PreLoader/app.js
@@ -1,13 +1,17 @@
 window.addEventListener('load', () => {
     const preload = document.querySelector(".preload");
-
     setTimeout(() => {
         preload.classList.add('preload-finish');
-    }, 2000); 
+        document.body.style.overflow = 'auto';
+    }, 2000);
 });
 
 document.querySelector('.btn').addEventListener('click', (event) => {
     event.preventDefault();
     const imageScreen = document.querySelector('.image-screen');
     imageScreen.classList.remove('hidden');
+    document.querySelector('.home').style.opacity = '0';
+    setTimeout(() => {
+        document.querySelector('.home').style.display = 'none';
+    }, 1000);
 });
