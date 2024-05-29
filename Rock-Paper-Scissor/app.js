9e2abcb360eaeb713f373fe3efdb4e1407abd8cb
Balla Kavya <ballakavya594@gmail.com>
2024-05-29 18:29:53 +0530

    Update

--- Rock-Paper-Scissor/app.js
+++ Rock-Paper-Scissor/app.js
@@ -2,10 +2,22 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
+
+const genCompChoice=()=>{
+    const options=["rock","paper","scissor"];
+    const idx=Math.floor(Math.random()*3);
+    
+}
+
+const playGame=(userchoice)=>{
+    console.log("user choice = ",userchoice);
+
+}
 choices.forEach((choice) => {
     console.log(choice);
     choice.addEventListener("click", () => {
         const userchoice=choice.getAttribute("id");
         console.log(`choice was clicked ${userchoice}`);
+        playGame(userchoice);
     });
 });
