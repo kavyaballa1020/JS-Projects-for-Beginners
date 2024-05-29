bc19492f2a37a074074d8a308a45ba3d57642079
Balla Kavya <ballakavya594@gmail.com>
2024-05-29 18:50:54 +0530

    Update

--- Rock-Paper-Scissor/app.js
+++ Rock-Paper-Scissor/app.js
@@ -3,50 +3,47 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
-const genCompChoice=()=>{
-    const options=["rock","paper","scissor"];
-    const idx=Math.floor(Math.random()*3);
-    return options[idx]; 
-
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const idx = Math.floor(Math.random() * 3);
+    return options[idx];
 }
-const showWinner = (userwin)=>{
-    if(userwin){
-        console.log("win"); 
-    }
-    else{
+
+const showWinner = (userwin) => {
+    if (userwin) {
+        console.log("win");
+    } else {
         console.log("lose");
     }
 }
-const drawGame =() =>{
+
+const drawGame = () => {
     console.log("game was draw");
-}  
-
-const playGame=(userchoice)=>{
-    console.log("user choice = ",userchoice);
-    const compchoice=genCompChoice();
-    console.log("comp choice = ",compchoice);
-
-    if(userchoice==compchoice){
-         drawGame();
-    }
-    else{
-        let userwin=true;
-        if(userchoice=="rock"){
-             userwin=compchoice=="paper"?false:true;
-        }
-        else if(userchoice=="paper"){
-            userwin=compchoice=="scissors"?false:true;
-        }
-        else{
-            userwin=compchoice=="rock"?false:true; 
-        }
-    }
-    showWinner(userwin);
-
 }
+
+const playGame = (userchoice) => {
+    console.log("user choice = ", userchoice);
+    const compchoice = genCompChoice();
+    console.log("comp choice = ", compchoice);
+
+    if (userchoice === compchoice) {
+        drawGame();
+    } else {
+        let userwin;
+        if (userchoice === "rock") {
+            userwin = compchoice === "paper" ? false : true;
+        } else if (userchoice === "paper") {
+            userwin = compchoice === "scissors" ? false : true;
+        } else {
+            userwin = compchoice === "rock" ? false : true;
+        }
+        showWinner(userwin);
+    }
+}
+
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
-        const userchoice=choice.getAttribute("id");
+        const userchoice = choice.getAttribute("id");
         playGame(userchoice);
     });
 });
