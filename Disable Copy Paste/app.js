235d12b15a8c6c8c7498a0392f706a3e429ad317
Balla Kavya <ballakavya594@gmail.com>
2024-06-10 09:33:01 +0530

    Update

--- Disable Copy Paste/app.js
+++ Disable Copy Paste/app.js
@@ -12,4 +12,29 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.right').addEventListener('paste', function(e) {
         e.preventDefault();
     });
+    // Allow pasting
+// var allowPaste = function(e){
+//     e.stopImmediatePropagation();
+//     return true;
+//   };
+//   document.addEventListener('paste', allowPaste, true);
+  
+//   // Allow context menu
+//   document.addEventListener('contextmenu', function(e) {
+//     e.stopImmediatePropagation();
+//     // Optionally, you can add additional logic here if needed
+//     return true;
+//   }, true);
+//   // Function to enable text selection
+// function enableTextSelection(element) {
+//     element.style.userSelect = 'auto';
+//     element.style.webkitUserSelect = 'auto';
+//     element.style.mozUserSelect = 'auto';
+//     element.style.msUserSelect = 'auto';
+// }
+
+// // Example usage: Enable text selection on the element with the 'left' class
+// const leftElement = document.querySelector('.left');
+// enableTextSelection(leftElement);
+
 });
