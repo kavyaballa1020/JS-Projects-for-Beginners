c4f1b0d947a97f973795d02a49c6c628913c70ae
Balla Kavya <ballakavya594@gmail.com>
2024-06-11 20:22:57 +0530

    Update

--- Side Navbar/script.js
+++ Side Navbar/script.js
@@ -1,4 +1,4 @@
-// scripts.js
+// script.js
 document.addEventListener("DOMContentLoaded", function() {
     const toggleBtn = document.querySelector(".toggle-btn");
     const sidenav = document.querySelector(".sidenav");
